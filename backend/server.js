bf8e5c503ea21d3a60c4d0be2390e16e44853943
disp <disp@sn.com>
2026-02-14 21:32:55 -0300

    x77

--- backend/server.js
+++ backend/server.js
@@ -88,7 +88,7 @@ CREATE TABLE IF NOT EXISTS users(
 await pool.query(`
 SELECT setval(
 'products_codigo_seq',
-COALESCE((SELECT MAX(codigo::Integer,0) FROM products),0)
+(SELECT COALESCE(MAX(codigo::INTEGER),0)+1 FROM products)
 );
 `);
 
